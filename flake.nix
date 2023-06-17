bbdb380be79fbd2b852b8421e72e64b5b45d7ccb
Kristoffer Dalby <kristoffer@dalby.cc>
2023-06-17 17:51:44 +0200

    add nix dev shell
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "kraweb - collection of convenience funcs for go http";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in {
+      # `nix develop`
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          golangci-lint
+          git
+          go_1_20
+        ];
+      };
+    });
+}
